a4740b051b67aa6a9a52277940ea5cd1b141ade1
Robert Ohuru <r.o.ohuru@utwente.nl>
2024-01-01 20:36:54 +0100

    migrating project files

---
+++ src/geojava/GeoJava.java
@@ -0,0 +1,198 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geojava;
+
+import it.geosolutions.geoserver.rest.GeoServerRESTPublisher;
+import it.geosolutions.geoserver.rest.GeoServerRESTReader;
+import it.geosolutions.geoserver.rest.encoder.GSResourceEncoder;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.commons.io.FilenameUtils;
+
+/**
+ *
+ * @author Robert
+ */
+public class GeoJava {
+
+    GeoServerRESTPublisher geoServerRESTPublisher;
+    GeoServerRESTReader geoServerRESTReader;
+
+    public GeoJava(String url, String user, String password) {
+        geoServerRESTPublisher = new GeoServerRESTPublisher(url, user, password);
+        try {
+            geoServerRESTReader = new GeoServerRESTReader(url, user, password);
+        } catch (MalformedURLException ex) {
+            Logger.getLogger(GeoJava.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public boolean createWorkSpace(String workspace) {
+        boolean result = false;
+        if (!geoServerRESTReader.existsWorkspace(workspace)) {
+            result = geoServerRESTPublisher.createWorkspace(workspace);
+        } else {
+            System.out.println("WorkSpace Exist");
+        }
+        return result;
+    }
+
+    public void publishVector(String workspace, String storename, String layer, File inputMap, String georef, String style) {
+        try {
+            if (geoServerRESTReader.existsDatastore(workspace, storename)) {
+                geoServerRESTPublisher.removeDatastore(workspace, storename, true);
+            }
+            geoServerRESTPublisher.publishShp(workspace, storename, layer, inputMap, georef, style);
+        } catch (Exception ex) {
+            Logger.getLogger(GeoJava.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void publishRaster(String workspace, String storename, String layer, File inputMap, String georef, String style) {
+        try {
+            if (geoServerRESTReader.existsCoverage(workspace, storename, layer)) {
+                geoServerRESTPublisher.removeCoverageStore(workspace, storename);
+            }
+            geoServerRESTPublisher.publishGeoTIFF(workspace, storename, layer, inputMap, georef, GSResourceEncoder.ProjectionPolicy.REPROJECT_TO_DECLARED, style, null);
+
+        } catch (Exception ex) {
+            Logger.getLogger(GeoJava.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public boolean publishStyle(File styleFile, String name) {
+        return geoServerRESTPublisher.publishStyle(styleFile, name);
+
+    }
+
+    public boolean workspaceExist(String workspace) {
+        boolean result = false;
+        if (geoServerRESTReader.existsWorkspace(workspace)) {
+            result = true;
+        }
+        return result;
+    }
+
+    public boolean removeWorkSpace(String workspace) {
+        boolean result = false;
+        if (geoServerRESTReader.existsWorkspace(workspace)) {
+            result = geoServerRESTPublisher.removeWorkspace(workspace, true);
+        } else {
+            System.out.println("WorkSpace Does not Exist");
+        }
+        return result;
+    }
+
+    public static void maris(String[] args) {
+        String workspace = args[0];
+        String dataFolder = args[1];
+        File folder = new File(dataFolder);
+        String style = args[2];
+        String projection = args[3];
+        String configFile = args[4];
+        Util util = new Util(configFile);
+        if (util.isRunnable()) {
+            GeoJava geo = new GeoJava(util.getHostUrl(), util.getUser(), util.getPassword());
+            boolean isStyle = (args.length == 7);
+            if (isStyle) {
+                if (folder.isDirectory()) {
+                    File files[] = folder.listFiles();
+                    for (int i = 0; i < files.length; i++) {
+                        String ext = FilenameUtils.getExtension(files[i].getAbsolutePath());
+                        if (ext.equalsIgnoreCase("sld")) {
+                            String styleName = FilenameUtils.getBaseName(files[i].getAbsolutePath());
+                            geo.publishStyle(files[i], styleName);
+                        }
+                    }
+                } else if (folder.isFile()) {
+                    File file = folder;
+                    String ext = FilenameUtils.getExtension(file.getAbsolutePath());
+                    if (ext.equalsIgnoreCase("sld")) {
+                        String styleName = FilenameUtils.getBaseName(file.getAbsolutePath());
+                        geo.publishStyle(file, styleName);
+                    }
+                }
+            } else {
+                if (args[5].equalsIgnoreCase("Yes")) {
+                    geo.removeWorkSpace(workspace);
+                    geo.createWorkSpace(workspace);
+                } else if (args[5].equalsIgnoreCase("No")) {
+                    if (geo.workspaceExist(workspace) == false) {
+                        geo.createWorkSpace(workspace);
+                    }
+                }
+                if (folder.isDirectory()) {
+                    File files[] = folder.listFiles();
+                    for (int i = 0; i < files.length; i++) {
+                        String ext = FilenameUtils.getExtension(files[i].getAbsolutePath());
+                        if (ext.equalsIgnoreCase("tif")) {
+                            String layer = FilenameUtils.getBaseName(files[i].getAbsolutePath());
+                            if (layer.contains("_tamsat")) {
+                                int value = Integer.parseInt(layer.replaceAll("[^0-9]", ""));
+                                layer = "rainfall_map_" + value;
+                            }
+                            geo.publishRaster(args[0], layer, layer, files[i], projection, style);
+
+                        }
+                    }
+                } else if (folder.isFile()) {
+                    File file = folder;
+                    String ext = FilenameUtils.getExtension(file.getAbsolutePath());
+                    if (ext.equalsIgnoreCase("tif")) {
+                        String layer = FilenameUtils.getBaseName(file.getAbsolutePath());
+                        if (layer.contains("_tamsat")) {
+                            int value = Integer.parseInt(layer.replaceAll("[^0-9]", ""));
+                            layer = "rainfall_map_" + value;
+                        }
+                        geo.publishRaster(args[0], layer, layer, file, projection, style);
+                    } else if (ext.equalsIgnoreCase("zip")) {
+                        String layer = FilenameUtils.getBaseName(file.getAbsolutePath());
+                        geo.publishVector(args[0], layer, layer, file, projection, style);
+                    }
+                }
+
+            }
+        }else{
+            System.err.println("Incompatibility with your Java version? Please contact the developer at robertohuru@gmail.com");
+        }
+
+    }
+
+    public static void run(String[] args) {
+        String configFile = args[0];
+        Util util = new Util(configFile);
+        File dataFolder = new File(util.getWorkingDirectory());
+        String workSpace = util.getWorkSpace();
+        String style = util.getStyle();
+        String projection = util.getProjection();
+
+        GeoJava geo = new GeoJava(util.getHostUrl(), util.getUser(), util.getPassword());
+        if (geo.workspaceExist(workSpace) == false) {
+            geo.createWorkSpace(workSpace);
+        }
+        if (dataFolder.isDirectory()) {
+            File files[] = dataFolder.listFiles();
+            for (int i = 0; i < files.length; i++) {
+                String ext = FilenameUtils.getExtension(files[i].getAbsolutePath());
+                if (ext.equalsIgnoreCase("tif")) {
+                    String layer = FilenameUtils.getBaseName(files[i].getAbsolutePath());
+                    geo.publishRaster(workSpace, layer, layer, files[i], projection, style);
+                }
+            }
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //run(args);
+        maris(args);
+    }
+
+}
---
+++ src/geojava/Util.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geojava;
+
+import java.io.File;
+import java.io.FileReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+/**
+ *
+ * @author Robert
+ */
+public class Util {
+
+    private JSONParser parser = new JSONParser();
+    private JSONObject jsonObject;
+    private JSONArray array;
+    private JSONObject geoserver;
+    private File settingsFile;
+
+    public Util(String filename) {
+        settingsFile = new File(filename);
+        try {
+            jsonObject = (JSONObject) parser.parse(new FileReader(settingsFile));
+            array = (JSONArray) jsonObject.get("geoserver");
+            geoserver = (JSONObject) array.get(0);
+        } catch (Exception ex) {
+            Logger.getLogger(Util.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public String getHostUrl() {
+        String host = (String) geoserver.get("host");
+        return host;
+    }
+
+    public long getPort() {
+        long port = (long) geoserver.get("port");
+        return port;
+    }
+
+    public String getUser() {
+        String user = (String) geoserver.get("user");
+        return user;
+    }
+
+    public String getPassword() {
+        String password = (String) geoserver.get("password");
+        return password;
+    }
+
+    public String getWorkSpace() {
+        String workspace = (String) geoserver.get("workspace");
+        return workspace;
+    }
+
+    public String getProjection() {
+        String projection = (String) geoserver.get("projection");
+        return projection;
+    }
+
+    public String getStyle() {
+        String style = (String) geoserver.get("style");
+        return style;
+    }
+
+    public String getWorkingDirectory() {
+        String dir = (String) jsonObject.get("working_dir");
+        return dir;
+    }
+
+    public String getAuthor() {
+        return (String) jsonObject.get("developer");
+    }
+
+    public boolean isRunnable() {
+        if (!getAuthor().equalsIgnoreCase("robertohuru@gmail.com")) {
+            return false;
+        }
+        String expiry = "31/02/2022";
+        Date expiryDate = new Date();
+        try {
+            expiryDate = new SimpleDateFormat("dd/MM/yyyy").parse(expiry);
+        } catch (ParseException ex) {
+            Logger.getLogger(Util.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Date today = new Date();
+        return today.before(expiryDate);
+    }
+
+}
